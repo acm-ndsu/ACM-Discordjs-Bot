12004922c793109a41cf7054ea4506fc5dcfbd31
Jean A. Eckelberg <fyrelynx11@gmail.com>
2023-12-14 16:30:43 -0600

    1 in 4 for Eric

--- src/commands/batcomb.js
+++ src/commands/batcomb.js
@@ -7,6 +7,8 @@ module.exports = {
     async execute(interaction) {
         if(interaction.user.id === '137754149210030080' && Math.floor(Math.random() * 4))
             await interaction.reply({content: 'You spelt it wrong, Adam!'})
+        else if(interaction.user.id === '138738295063576576' && Math.floor(Math.random() * 4))
+            await interaction.reply({content: 'You spelt it wrong, Eric!'})
         else
             await interaction.reply({files: [{attachment: 'persistence/batcomb.png'}]})
     }
